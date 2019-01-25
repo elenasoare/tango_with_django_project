adf53d2895027f6064dfc186081a98f62598e944
Elena Soare <2309307s@student.gla.ac.uk>
2019-01-25 10:02:14 +0000

    sixth commit

--- populate_rango.py
+++ populate_rango.py
@@ -6,6 +6,19 @@ import django
 django.setup()
 from rango.models import Category, Page
 
+def add_page(cat, title, url, views = 0):
+    p = Page.objects.get_or_create(category=cat, title = title)[0]
+    p.url = url
+    p.views = views
+    p.save()
+    return p
+def add_cat(name, views = 0, likes = 0):
+    c = Category.objects.get_or_create(name = name)[0]
+    c.likes = likes
+    c.views = views
+    c.save()
+    return c
+
 def populate():
     # create lists of dictionaries containing the pages
     #we want to add in each category
@@ -60,19 +73,6 @@ def populate():
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print ("- {0} - {1}".format(str(c), str(p)))
-
-def add_page(cat, title, url, views = 0):
-    p = Page.objects.get_or_create(category=cat, title = title)[0]
-    p.url = url
-    p.views = views
-    p.save()
-    return p
-def add_cat(name, views = 0, likes = 0):
-    c = Category.objects.get_or_create(name = name)[0]
-    c.likes = likes
-    c.views = views
-    c.save()
-    return c
 
 #start execution
 
