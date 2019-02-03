267aa5a8c5a56ddb35064c8119881eea2e475d07
Elena Soare <2309307s@student.gla.ac.uk>
2019-02-03 21:59:55 +0000

    ninth commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import UserProfile
 from rango.models import Category, Page
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,3 +9,4 @@ class PageAdmin(admin.ModelAdmin):
     
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
